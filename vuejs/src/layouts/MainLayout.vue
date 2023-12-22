18eba4720f05208f569606c45b4240a62f2ef8cc
Adriann Guedes <adrianguedes902@gmail.com>
2023-12-22 10:37:53 -0300

    first commit

---
+++ vuejs/src/layouts/MainLayout.vue
@@ -0,0 +1,89 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header class=" flex justify-between items-center q-pa-md">
+      <q-icon name="arrow_back_ios" size="20px"></q-icon>
+      <q-toolbar-title class="text-center ">
+          Master Learning
+        </q-toolbar-title>
+
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-1"
+    >
+      <q-list>
+        <q-item-label
+          header
+          class="text-grey-8"
+        >
+          Menu
+        </q-item-label>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksData = [
+  {
+    title: 'Linkedim',
+    caption: 'Aqui você será direcionado ao meu perfil',
+    icon: 'school',
+    link: 'https://www.linkedin.com/in/adrian-gds-0133a0234/'
+  },
+  {
+    title: 'GitHub',
+    caption: 'Aqui tem mais projetos...',
+    icon: 'code',
+    link: 'https://github.com/AdriannGuedes'
+  },
+]
+
+export default {
+  name: 'MainLayout',
+  components: {
+    EssentialLink
+  },
+  data () {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: linksData
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+.q-header{
+  background-color: 	#87CEEB;
+}
+
+.q-page-container {
+  padding-top: 65px !important;
+}
+
+</style>
---
+++ vuejs/src/pages/Index.vue
@@ -0,0 +1,57 @@
+<template>
+  <q-page class="container flex column justify-around flex-center">
+    <q-img
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_UQh51rKjZ66Ii_7lJKyI7ZRDspXIdrbPfA&usqp=CAU"
+      spinner-color="red"
+      style="height: 220px; max-width: 230px"
+    />
+    <h4 class="title q-ma-sm text-center">{{ phrases[index] }}</h4>
+    <q-btn class="button" rounded  label="Gerar Frase"  @click="randomPosition" />
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "Frase do dia",
+  data() {
+    return {
+      phrases: [
+        "Os carros são minha terapia sobre rodas.",
+        "O carro é a maior invenção do homem.",
+        "Quatro rodas, um motor, uma paixão que me faz sentir vivo.",
+        "Eu não coleciono carros, eu coleciono histórias de aventura.",
+      ],
+      index: 0,
+    };
+  },
+
+  methods: {
+    randomPosition() {
+      let index = parseInt(Math.random() * 4 + 0);
+      console.log(index);
+      this.index = index;
+    },
+  },
+
+  beforeMount() {
+    this.randomPosition();
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+.container {
+  background-color: 	#87CEEB;
+}
+
+.title {
+  color: #fff;
+}
+
+.button {
+  background-color: #262525;
+  color: #fff;
+  width: 90%;
+  height: 50px;
+}
+</style>
